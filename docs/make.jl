fad7cc6484539669bfdbb32999e63b66f356d8ee
Ben Ide <ben@bencide.com>
2023-04-25 16:32:02 -0400

    reduce some things

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(Test, :DocTestSetup, :(using Test); recursive=true)
 makedocs(;
     modules=[Test],
     authors="Ben Ide <ben@bencide.com>",
-    repo="https://github.com/benide/Test.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/benide/Test/blob/{commit}{path}#{line}",
     sitename="Test.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://benide.github.io/Test.jl",
+        canonical="https://benide.github.io/Test",
         edit_link="master",
         assets=String[],
     ),
@@ -20,6 +20,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/benide/Test.jl",
+    repo="github.com/benide/Test",
     devbranch="master",
 )
